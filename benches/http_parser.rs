de5a8bb582187bf23bcacbaa2fa80acc38156f33
Mesut Tasci <mst.tasci@gmail.com>
2021-10-31 00:43:59 +0300

    perf(http-parser): add request parsing benchmark

---
+++ benches/http_parser.rs
@@ -0,0 +1,18 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use restman::http::parser::request;
+use nom_locate::LocatedSpan;
+
+fn request_parsing_benchmark(c: &mut Criterion) {
+    let input = LocatedSpan::new("GET /index.html HTTP/1.1\r\n\
+        Content-type: application/json\r\n\
+        Authorization: bearer token\r\n\
+        \r\n\
+        {\"foo\": \"bar\"}\r\n");
+
+    c.bench_function("parse http request", |b| b.iter(|| {
+        request(input).unwrap();
+    }));
+}
+
+criterion_group!(benches, request_parsing_benchmark);
+criterion_main!(benches);
