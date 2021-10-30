73ff719a0a76e0f985a519f426a497e48608b076
Mesut Tasci <mst.tasci@gmail.com>
2021-10-30 16:42:15 +0300

    refactor(http-parser): refactor tests

--- src/http/parser/test.rs
+++ src/http/parser/test.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod test {
-    use crate::http::parser::{Header, header_line, request_line, RequestLine, Version, Request, Method, request, MessageBody, request_body, block_parser};
+    use crate::http::parser::{block_parser, Header, header_line, MessageBody, Method, Request, request, request_body, request_line, RequestLine, Version};
 
     #[test]
     fn request_line_test() {
@@ -12,20 +12,15 @@ mod test {
             version: Version::V11,
         };
 
-        assert_eq!(result, Ok((&[][..], expected)));
+        assert_eq!(result, Ok((&b""[..], expected)));
     }
 
     #[test]
     fn request_line_without_http_version() {
         let input = b"GET /index.html\r\n";
-        let result = request_line(input);
-        let expected = RequestLine {
-            method: b"GET",
-            path: b"/index.html",
-            version: Version::V11,
-        };
+        let (_, l) = request_line(input).unwrap();
 
-        assert_eq!(result, Ok((&[][..], expected)));
+        assert_eq!(l.version, Version::V11);
     }
 
     #[test]
@@ -46,17 +41,17 @@ mod test {
     fn request_parser_test() {
         let input = b"GET /index.html HTTP/1.1\r\n\
         Content-type: application/json\r\n\
-        Authorization: foo\r\n\
+        Authorization: bearer token\r\n\
         \r\n\
         {\"foo\": \"bar\"}\r\n";
 
         let result = request(input).unwrap();
 
-        let expected = Request{
+        let expected = Request {
             method: Method::Get,
             path: "/index.html".to_string(),
             version: Version::V11,
-            headers: vec![Header{ name: b"Content-type", value: b"application/json" }, Header{ name: b"Authorization", value: b"foo" }],
+            headers: vec![Header { name: b"Content-type", value: b"application/json" }, Header { name: b"Authorization", value: b"bearer token" }],
             body: MessageBody::Bytes(b"{\"foo\": \"bar\"}"),
         };
 
@@ -65,9 +60,10 @@ mod test {
 
     #[test]
     fn request_body_parser_test() {
-        let input = b"\r\n{\"foo\": \"bar\"}\r\n";
+        let input = b"\r\nfoo=bar\r\n";
         let (i, u) = block_parser(input, b"\r\n", b"\r\n").unwrap();
-        println!("{:?}", std::str::from_utf8(u));
+
+        assert_eq!(&b"foo=bar"[..], u);
     }
 
     #[test]
@@ -75,7 +71,7 @@ mod test {
         let input = b"GET /index.html HTTP/1.1\r\n\r\nfoo=bar\r\n";
         let result = request(input).unwrap();
 
-        let expected = Request{
+        let expected = Request {
             method: Method::Get,
             path: "/index.html".to_string(),
             version: Version::V11,
